26e614db49ff2848a5f9c159dd663cf39e3cfdda
jimallengroup <115572232+jimallengroup@users.noreply.github.com>
2023-02-24 09:46:01 -0500

    init

---
+++ .cache/sync-requires.js
@@ -0,0 +1,23 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/jagsmgs/Sites/mei/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/jagsmgs/Sites/mei/src/pages/404.js")),
+  "component---src-pages-about-js": preferDefault(require("/Users/jagsmgs/Sites/mei/src/pages/about.js")),
+  "component---src-pages-blog-2-js": preferDefault(require("/Users/jagsmgs/Sites/mei/src/pages/blog-2.js")),
+  "component---src-pages-blog-3-js": preferDefault(require("/Users/jagsmgs/Sites/mei/src/pages/blog-3.js")),
+  "component---src-pages-blog-js": preferDefault(require("/Users/jagsmgs/Sites/mei/src/pages/blog.js")),
+  "component---src-pages-blog-single-js": preferDefault(require("/Users/jagsmgs/Sites/mei/src/pages/blog-single.js")),
+  "component---src-pages-contact-js": preferDefault(require("/Users/jagsmgs/Sites/mei/src/pages/contact.js")),
+  "component---src-pages-index-2-js": preferDefault(require("/Users/jagsmgs/Sites/mei/src/pages/index-2.js")),
+  "component---src-pages-index-3-js": preferDefault(require("/Users/jagsmgs/Sites/mei/src/pages/index-3.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/jagsmgs/Sites/mei/src/pages/index.js")),
+  "component---src-pages-portfolio-details-js": preferDefault(require("/Users/jagsmgs/Sites/mei/src/pages/portfolio-details.js")),
+  "component---src-pages-portfolio-js": preferDefault(require("/Users/jagsmgs/Sites/mei/src/pages/portfolio.js")),
+  "component---src-pages-service-details-js": preferDefault(require("/Users/jagsmgs/Sites/mei/src/pages/service-details.js")),
+  "component---src-pages-service-js": preferDefault(require("/Users/jagsmgs/Sites/mei/src/pages/service.js"))
+}
+
